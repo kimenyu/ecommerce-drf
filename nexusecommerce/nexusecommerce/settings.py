dd18d756dee049130d223e704d98675379acfa3a
kimenyu <njorogekimenyu@gmail.com>
2025-03-09 10:39:51 +0300

    feat: Dockerize project and set up PostgreSQL database
    - Added `Dockerfile` to containerize the Django application
    - Created `docker-compose.yml` for simplified service management
    - Configured PostgreSQL as the database for the application
    - Updated settings to connect Django to PostgreSQL in Docker
    - Successfully ran the application in a Docker container
    
    This commit enables easy deployment and local development using Docker with PostgreSQL.

--- nexusecommerce/nexusecommerce/settings.py
+++ nexusecommerce/nexusecommerce/settings.py
@@ -90,11 +90,18 @@ WSGI_APPLICATION = 'nexusecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=f"postgres://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}")
 }
 
 #cors headers
